a01ffa15859e84359ca8ec2f46775384376ca05b
youssefabdelmaged <youssefabdelmaged50@gmail.com>
2025-05-27 23:41:02 +0300

    validation name

--- DBMS.sh
+++ DBMS.sh
@@ -1,1 +1,16 @@
 #! /bin/bash
+
+# directory for storing the databases
+DB_DIR="databases"
+
+mkdir -p "$DB_DIR"
+
+#validate the database name and the table name
+validate_name(){
+    local name ="$1"
+    if [[ !"name" = ~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]; then
+        echo "Invalid name: $name. Names must start with a letter and can only contain letters, numbers, and underscores."
+        return 1
+    fi
+    return 0
+}
