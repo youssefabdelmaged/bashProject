5b066183f16e4b8bd421949cbb65e19b5c8c46f9
Ahmedmanso4r <UGS.141184@ci.suez.edu.eg>
2025-05-29 23:06:41 +0300

    # Main Menu

--- DBMS.sh
+++ DBMS.sh
@@ -466,3 +466,26 @@ connect_to_database() {
         esac
     done
 }
+
+# Main Menu
+while true; do
+    echo -e "\nDBMS Main Menu"
+    echo "1. Create Database"
+    echo "2. List Databases"
+    echo "3. Connect To Database"
+    echo "4. Drop Database"
+    echo "5. Exit"
+    read -p "Select an option: " option
+
+    case $option in
+    1) create_database ;;
+    2) list_databases ;;
+    3) connect_to_database ;;
+    4) drop_database ;;
+    5)
+        echo "Exiting..."
+        exit 0
+        ;;
+    *) echo "Invalid option." ;;
+    esac
+done
